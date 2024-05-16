e7537587e79e5cd9f18311b8078b53041ee34b11
Wallace143900 <hiperguardi@gmail.com>
2024-05-16 14:04:18 -0300

    Adicionar roteamento para páginas de Login e Home
    - Implementar roteamento usando BrowserRouter, Routes e Route da biblioteca react-router-dom
    - Definir rotas para as páginas de Login e Home
    - Configurar uma rota curinga para redirecionar para a página de Login em caso de rota inválida

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/pages/Login';
+import Home from './components/pages/home';
 
 
-function App() {
-
+const App: React.FC = () => {
   return (
-    <></>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="*" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
